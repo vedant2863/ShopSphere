6069010582c5a8abf1a73ed884faf46e4c71a7de
vedant2863 <jvedant727@gmail.com>
2025-01-06 10:10:15 +0530

    product_list url and path create

--- product/views.py
+++ product/views.py
@@ -3,3 +3,10 @@ from .models import Product
 
 # Create your views here.
 
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product/product_list.html', {'products':products})
+
+def product_detail(request, id, slug):
+    product = Product.objects.get(id=id)
+    return render(request, 'product/product_detail.html', {'product':product})
