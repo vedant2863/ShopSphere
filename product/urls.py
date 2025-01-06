7169b450adbb17f1e50c030e7935dddf935c61d6
vedant2863 <jvedant727@gmail.com>
2025-01-06 10:11:07 +0530

    product_detail url and view create

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.product_list, name='product_list'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -8,5 +9,5 @@ def product_list(request):
     return render(request, 'product/product_list.html', {'products':products})
 
 def product_detail(request, id, slug):
-    product = Product.objects.get(id=id)
-    return render(request, 'product/product_detail.html', {'product':product})
+    product = get_object_or_404(Product, id=id, slug=slug)
+    return render(request, 'product/product_detail.html', {'product': product})
