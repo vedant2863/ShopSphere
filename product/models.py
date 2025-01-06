a0c8272ae25e44f839046b13a6e496ee085c67ce
vedant2863 <jvedant727@gmail.com>
2025-01-06 10:07:13 +0530

    category and product model setup

---
+++ product/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,  on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='product')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('products:product_detail', kwargs={'id':self.id, 'slug':self.slug})
+    
